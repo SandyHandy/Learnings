d4f8c4207a5c11a7a67f777aa2fa9bb49d788504
sandeephavaragi <sandeep.havaragi@gmail.com>
2016-11-22 21:33:21 -0800

    Updated the exercise

--- Java8InAction-master/src/main/java/lambdasinaction/chap5/my/PuttingIntoPractice.java
+++ Java8InAction-master/src/main/java/lambdasinaction/chap5/my/PuttingIntoPractice.java
@@ -2,7 +2,10 @@ package lambdasinaction.chap5.my;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
 import static java.util.stream.Collectors.toList;
+import static java.util.Comparator.comparing;
 
 public class PuttingIntoPractice {
 
@@ -22,17 +25,67 @@ public class PuttingIntoPractice {
 				);
 		
 		
+//		1. Find all transactions in the year 2011 and sort them by value (small to high).
+		
+		System.out.println("**** All the 2011 transaction sorted by value ****");
+		List<Transaction> allTran2011 = transactions.stream()
+										.filter(d->d.getYear()==2011)
+										.sorted(comparing(Transaction::getValue))
+										.collect(toList());
+		allTran2011.forEach(System.out::println);
+		
+//		2. What are all the unique cities where the traders work?
+		
+		System.out.println("**** All Unique cities where the Trader worked ****");
+		List<String> uniqeCities = transactions.stream()
+									.map(Transaction::getTrader)
+									.map(Trader::getCity)
+									.distinct()
+									.collect(toList());
+		uniqeCities.forEach(System.out::println);
+		
+//		3. Find all traders from Cambridge and sort them by name.
+		System.out.println("**** Traders from Cambridge and sort them by name ****");
+		
+		List<Trader> traderCambridge = transactions.stream()
+				.map(Transaction::getTrader)
+				.filter(t->t.getCity().equals("Cambridge"))
+				.sorted(comparing(Trader::getName))
+				.collect(toList());
+		traderCambridge.forEach(System.out::println);
+		
+//		4. Return a string of all traders’ names sorted alphabetically.
+		System.out.println("**** Return a string of all traders’ names sorted alphabetically ****");
+		
+		String traderNames = transactions.stream()
+						.map(Transaction::getTrader)
+						.sorted(comparing(Trader::getName))
+						.map(Trader::getName)
+						.reduce("",(n1,n2)->n1 + n2);
+						
+		System.out.println(traderNames);				
+		
+//		5. Are any traders based in Milan?
+		System.out.println("**** Are any traders based in Milan? ****");
+		
+//		Trader =get all the trader, filer, findAny
+		Optional<Trader> miladTrader = transactions.stream().map(Transaction::getTrader).filter(trader->trader.getCity().equals("Milan")).findAny();
+		
+//		6. Print all transactions’ values from the traders living in Cambridge.
+//		7. What’s the highest value of all the transactions?
+//		8. Find the transaction with the smallest value.
+		
 		 /*Find all transactions in the year 2011 and sort them by value (small to high).*/
 //		List<Transaction> trasaction2011 = transactions.stream().filter(d->d.getYear()==2011).sorted().collect(toList());
 //		trasaction2011.forEach(System.out::println);
 //		
 		 /*What are all the unique cities where the traders work?*/
-		List<String> uniqueCities = transactions.stream().map(d->d.getTrader().getCity()).distinct().collect(toList());
+/*		List<String> uniqueCities = transactions.stream().map(d->d.getTrader().getCity()).distinct().collect(toList());
 		uniqueCities.forEach(System.out::println);
 		
 //		Find all traders from Cambridge and sort them by name.
 		List<Trader> traderFromCam = transactions.stream().map(d->d.getTrader()).filter(d->d.getCity().equals("Cambridge")).collect(toList());
-		traderFromCam.forEach(System.out::println);
+		traderFromCam.forEach(System.out::println);*/
 		
 	}
 }
