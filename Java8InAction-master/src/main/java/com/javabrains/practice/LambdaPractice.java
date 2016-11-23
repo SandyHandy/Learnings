b58bc1b3b2cdef243a4310fecacee125a40711bf
sandeephavaragi <sandeep.havaragi@gmail.com>
2016-11-22 21:34:40 -0800

    initial checkin

---
+++ Java8InAction-master/src/main/java/com/javabrains/practice/LambdaPractice.java
@@ -0,0 +1,13 @@
+package com.javabrains.practice;
+
+import java.util.function.Predicate;
+
+public class LambdaPractice {
+
+	
+	
+	public static void caculate(Predicate<String> p){
+		
+	p.toString();   
+	}
+}
